3b57347659a894092b26af29e41b3dd27c5b6faa
Lichobor <mihaylown@yandex.ru>
2021-04-09 10:16:55 +0300

    удалил dist

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ const twoItem = oneItem.nextElementSibling;
 const elem = twoItem.getBoundingClientRect();
 console.log(elem);
 console.log(elem.height) */
+
+/* const myPromise = new Promise(
+    (resolve) => {
+        setTimeout (
+            () => {
+                console.log('Привет, я функция-исполнитель!')
+            }, 2000
+        )
+    }
+);
+ */
+
+console.log(this);
